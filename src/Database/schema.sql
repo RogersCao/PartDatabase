aa947a7bf7e517a3f0fb7f56ff7cdd84892e6e42
Jianxuan <rogerscao0623@gmail.com>
2020-12-29 16:44:01 +0000

    sql query insert schema done

--- src/Database/schema.sql
+++ src/Database/schema.sql
@@ -1,32 +1,37 @@
--- DROP TABLE Record;
--- DROP TABLE Part;
--- DROP TABLE Customer;
--- DROP TABLE Category;
+CREATE TABLE IF NOT EXISTS Category ( CategoryID uuid default random_uuid() PRIMARY KEY,  Name VARCHAR(50) NOT NULL);
 
-CREATE TABLE IF NOT EXISTS Category (CategoryID uuid default random_uuid() PRIMARY KEY, Name VARCHAR(50) NOT NULL);
-
-CREATE TABLE IF NOT EXISTS Customer (CustomerID uuid default random_uuid() PRIMARY KEY, Name VARCHAR(50) NOT NULL);
+CREATE TABLE IF NOT EXISTS Customer ( CustomerID uuid default random_uuid() PRIMARY KEY,  Name VARCHAR(50) NOT NULL);
 
 CREATE TABLE IF NOT EXISTS Part (
-	PartID VARCHAR(50) NOT NULL PRIMARY KEY,
-	ModelNum VARCHAR(50) NOT NULL,
-	Name VARCHAR(50) NOT NULL,
-	CategoryID uuid NOT NULL,
-	Stock int NOT NULL,
-	foreign key (CategoryID) references Category(CategoryID)
+    PartID VARCHAR(50) NOT NULL PRIMARY KEY,
+    ModelNum VARCHAR(50) NOT NULL,
+    Name VARCHAR(50) NOT NULL,
+    CategoryID uuid NOT NULL,
+    Stock int NOT NULL,
+    foreign key (CategoryID) references Category(CategoryID)
 );
 
 CREATE TABLE IF NOT EXISTS Record (
-	RecordID uuid default random_uuid() PRIMARY KEY,
-	Date DATE NOT NULL,
-	CustomerID uuid NOT NULL,
-	PartID VARCHAR(50) NOT NULL,
-	QuantityIN int NOT NULL,
-	QuantityOUT int NOT NULL,
-	CurrentStock int NOT NULL,
-	Remark VARCHAR(300),
-	foreign key (CustomerID) references Customer(CustomerID),
-	foreign key (PartID) references Part(PartID)
+    RecordID uuid default random_uuid() PRIMARY KEY,
+    Date DATE NOT NULL,
+    CustomerID uuid NOT NULL,
+    PartID VARCHAR(50) NOT NULL,
+    QuantityIN int NOT NULL,
+    QuantityOUT int NOT NULL,
+    CurrentStock int NOT NULL,
+    Remark VARCHAR(300),
+    foreign key (CustomerID) references Customer(CustomerID),
+    foreign key (PartID) references Part(PartID)
 );
 
-INSERT INTO Category VALUES('');
+DROP TABLE Record;
+DROP TABLE Part;
+DROP TABLE Customer;
+DROP TABLE Category;
+
+Select * from Category;
+Select * from Customer;
+Select * from Part;
+Select * from Record;
+
+DELETE FROM Part;
